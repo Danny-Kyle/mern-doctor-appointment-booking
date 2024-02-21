c3d14abee5bcd842fb949e2b3d61ba59b1a94ee4
Danny-Kyle <dikedaniel7917@gmail.com>
2024-02-21 12:54:20 +0100

    user model schema defined

--- server/models/UserModel.ts
+++ server/models/UserModel.ts
@@ -1,1 +1,37 @@
 import mongoose  from "mongoose";
+const UserSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isDoctor: {
+        type: Boolean,
+        default: false
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    seenNotifications: {
+        type: Array,
+        default: []
+    },
+    unseenNotifications: {
+        type: Array,
+        default: []
+    },
+},
+{
+    timestamps: true,
+});
+
+const userModel = mongoose.model("Users", UserSchema);
+module.exports = userModel;
