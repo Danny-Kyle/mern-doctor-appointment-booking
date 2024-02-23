05e3b8581e2757bf255c13f8b1d7c3c6166018e7
Danny-Kyle <dikedaniel7917@gmail.com>
2024-02-23 15:29:35 +0100

    pretty sure i jst made the config/db.js file obsolete

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 import express from "express"
+import { configDotenv } from "dotenv";
+import { mongoose } from "mongoose";
 import colors from "cors"
 import path from "path"
-import connectDB from "../config/db"
+// import connectDB from "../config/db"
 // const dbConfig = require("../config/db")
 const app = express();
 app.use(express.json());
-require("dotenv").config();
+configDotenv();
 
-connectDB();
+const connection = async () =>{
+    try{
+        const connection = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`Mongoose Connected securely ${connection.connection.host}`.cyan.underline.bold);
+    }catch(error){
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+
 
 const port = process.env.PORT || 3135;
 
